923233ea9b049a5238f843e448392ac5932a0377
csj <sjchen@gk-estor.com>
2022-04-29 12:01:21 +0800

    quickstart

---
+++ src/main/java/com/example/pojo/Fruit.java
@@ -0,0 +1,20 @@
+package com.example.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author csijun
+ * @Date 2022/4/29
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fruit {
+
+    private String name;
+
+    private String description;
+
+}
